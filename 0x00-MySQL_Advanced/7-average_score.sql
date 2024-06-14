dcbc10320751d954b9d87aa3adcfeb9a68ce5532
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-06-14 00:46:00 +0000

    task#7: average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,19 @@
+-- create the stored procedure `ComputeAverageScoreForUser`
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+
+    -- calculate the average score for the given user
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = user_id;
+
+    -- update the average score in the users table
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
+END$$
+
+DELIMITER ;
