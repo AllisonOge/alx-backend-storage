025efd2221061ea680e7fce4ce15578bbb154f14
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-06-19 11:13:47 -0700

    task#8: list all documents in python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,17 @@
+#/usr/bin/env python3
+"""
+Module that defines the `list_all` function
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection.
+
+    Args:
+        mongo_collection: pymongo collection object
+
+    Returns:
+        List of documents, or an empty list if no documents are found
+    """
+    return list(mongo_collection.find())
